c3866b63ad35d928312a8e7cb0c0c1cfa33b63ca
Dulakshi <dulakshigamma@gmail.com>
2026-08-01 21:15:39 +0530

    Add Docker support (Dockerfiles + docker-compose), fix RestClient to use configurable driver-service URL

--- reservation-service/src/main/java/com/parkease/reservationservice/config/RestClientConfig.java
+++ reservation-service/src/main/java/com/parkease/reservationservice/config/RestClientConfig.java
@@ -3,14 +3,18 @@ package com.parkease.reservationservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class RestClientConfig {
 
+	@Value("${driver.service.url}")
+	private String driverServiceUrl;
+	
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
-                .baseUrl("http://localhost:8081")
+                .baseUrl(driverServiceUrl)
                 .build();
     }
 }
